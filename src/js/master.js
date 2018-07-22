e4a9dab9e73e75ccbc53f1b8da10c99f3df0fd6d
Ryan <ryanjmack21@gmail.com>
2018-07-22 08:24:15 -0400

    Add functionality to determine cell colors dynamically

--- src/js/master.js
+++ src/js/master.js
@@ -7,18 +7,19 @@ const averageFahrenheit = 57;
 const convertCelsius = (celsius) => Math.round((celsius * 1.8) * 100) / 100;  // converts C to F rounding to nearest hundreth
 const endYear = 2018;
 const height = 500;
+const legendPadding = 60;
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'June', 'July', 'September', 'October', 'November', 'December'];
 const startYear = 1880;
 const width = 1300;
-const xAxisPadding = 100;
+const xAxisPadding = 150;
 const yAxisPadding = 75;
 
 // create the svg element
 const svg = d3.select('main')
   .append('svg')
   .attr('width', width)
-  .attr('height', height);
-
+  .attr('height', height)
+  .attr('overflow', 'visible');
 
 // Scales
 // x axis is the year
@@ -32,9 +33,28 @@ const yScale = d3.scaleLinear()
   .range([height - xAxisPadding, 0]);
 
 
+// find min and max variance in the dataset
+const maxVariance = dataset.reduce((a, b) => a = (a < b.variance) ? b.variance : a, -Infinity);
+const minVariance = dataset.reduce((a, b) => a = (a > b.variance) ? b.variance : a, Infinity);
+
+// colors used in the graph
+const colors = ['#0D095B', '#3249AB', '#127FC0', '#03CFD7', '#01C000', '#01C000', '#F8EF1C', '#FEBB11', '#F26322', '#FE0100'];
+// minVariance + (colorStep * i) will give us color index
+const colorStep = (maxVariance + Math.abs(minVariance)) / colors.length;
+
+// called when populating the graph with data
+const getColor = (variance) => {
+  let i = 0;
+  while (((i + 1) * colorStep) + minVariance < variance) {
+    i++;
+  }
+  return colors[i];
+};
+
+
 // used for the bars in the visualization.
-const barHeight = (height - xAxisPadding) / (months.length + 1); // 12 vertical bars (months)
-const barWidth = (width - yAxisPadding) / (endYear - startYear); // 2018 - 1880 = 138 bars horizontally (years)
+const barHeight = (((height - xAxisPadding) / months.length) + 2); // 12 vertical bars (months); space closer together by adding 2
+const barWidth = (width / (endYear - startYear)) - 1; // 2018 - 1880 = 138 bars horizontally (years); add spacing by subtracting 1
 
 // populate the svg with data
 svg.selectAll('rect')
@@ -45,6 +65,6 @@ svg.selectAll('rect')
   .attr('y', data => yScale(data.month))
   .attr('width', barWidth)
   .attr('height', barHeight)
-  .attr('fill', 'salmon')
+  .attr('fill', data => getColor(data.variance))
   .append('title')
   .text(data => `${months[data.month - 1]}, ${data.year}\nVariance: ${data.variance}°C / ${convertCelsius(data.variance)}°F`);
